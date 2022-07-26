2e16f7d445226943f731e67ab87f6c702ec3d3d2
Sarat862 <sarat862@gmail.com>
2022-07-27 00:43:28 +0300

    Виконав друге завдання

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle'
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+const KEY_STORAGE = "videoplayer-current-time"
+const currentTime = localStorage.getItem(KEY_STORAGE)
+
+player.on('timeupdate', throttle(onTimeSet, 1000));
+
+function onTimeSet({seconds}) {
+    localStorage.setItem(KEY_STORAGE, seconds);
+}
+
+if (currentTime) {
+    player.setCurrentTime(currentTime);
+}
