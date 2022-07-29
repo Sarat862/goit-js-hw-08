3b63359d840e87b667710d19f5fc3167dc3330b1
Sarat862 <sarat862@gmail.com>
2022-07-30 00:13:32 +0300

    Виконав завдання №3 домашньої роботи

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,46 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector(".feedback-form");
+const input = document.querySelector(".feedback-form input");
+const textarea = document.querySelector(".feedback-form textarea");
+const LOCALSTORAGE_KEY = "feedback-form-state";
+
+let objectFormValue = {};
+
+updatePageBrowser()
+    
+form.addEventListener("input", throttle(onFormInput, 500));
+form.addEventListener("submit", onFormSubmit);
+
+function onFormInput(event) {
+    const email = form.elements.email.value;
+    const message = form.elements.message.value;
+    objectFormValue = { email, message };
+    // objectFormValue[event.target.name] = event.target.value;
+    localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(objectFormValue));
+}
+
+function onFormSubmit(event) {
+    event.preventDefault();
+    objectFormValue.email = input.value;
+    objectFormValue.message = textarea.value;
+
+    if (input.value === "" || textarea.value === "") {
+        alert("Заповніть, будь ласка, пусті поля!")
+    } else {
+        console.log(objectFormValue);
+        event.currentTarget.reset();
+        localStorage.removeItem(LOCALSTORAGE_KEY);
+    }
+}
+  
+function updatePageBrowser() {
+    const objStorage = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY));
+    // console.log(objStorage);
+
+    if (objStorage) {
+        input.value = objStorage.email;
+        textarea.value = objStorage.message;
+    }    
+}
+  
